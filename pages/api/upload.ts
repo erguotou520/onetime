e518496bd0670151915e188c3b9e36356d4bb9ae
erguotou <erguotou525@gmail.com>
2022-12-06 20:57:02 +0800

    feat: update upload

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -1,26 +1,41 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { storage } from './_deta'
-import multiparty from 'multiparty'
+import formidable from 'formidable'
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+};
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log(req)
-  const form = new multiparty.Form()
-  const files = await new Promise((resolve) => {
-    form.parse(req, (err, fields, files) => {
-      console.log(err, fields, files)
-      resolve(files)
+  const form = new formidable.IncomingForm();
+  const file = await new Promise<formidable.File | null>((resolve) => {
+    form.parse(req, async function (err, fields, files) {
+      if (!err) {
+        const _files = Object.values(files)[0]
+        if (Array.isArray(_files)) {
+          resolve(_files[0])
+        } else {
+          resolve(_files)
+        }
+      }
+      return null
     })
   })
+  if (!file) {
+    return res.status(400).send({ err: 'No file selected' })
+  }
   const randomName = `${+new Date()}_${Math.random().toString(36).substring(2)}`
-  console.log(randomName)
+  
   try {
-    // const ret = await storage.put(randomName, files[0])
-    res.status(200).json({ url: '' })
+    const ret = await storage.put(`${randomName}/${file.originalFilename}`, { path: file.filepath, contentType: file.mimetype ?? undefined })
+    res.status(200).json({ url: ret })
   } catch (error) {
-    res.status(500)
+    res.status(500).end()
   }
 }
